7857676387c3ed90831bc3403fe47bffe180e6a7
Chad Chang <chad.chang@verizonmedia.com>
2021-02-08 22:25:38 +0800

    make sure init RegionSelectorManager with data loader

--- CCRegionSelector/Tests/RegionSelectorManagerTests.swift
+++ CCRegionSelector/Tests/RegionSelectorManagerTests.swift
@@ -9,14 +9,37 @@
 import XCTest
 @testable import CCRegionSelector
 
+protocol RegionDataLoader {
+
+}
+
 class RegionSelectorManager {
+    private let dataLoader: RegionDataLoader
 
-
+    init(dataLoader: RegionDataLoader) {
+        self.dataLoader = dataLoader
+    }
 }
 
 class RegionSelectorManagerTests: XCTestCase {
     func test_init_doestNotNil() {
-        let sut = RegionSelectorManager()
+        let (sut, _) = makeSUT()
         XCTAssertNotNil(sut)
     }
+
+    // MARK: - Helpers
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: RegionSelectorManager, client: RegionDataLoader) {
+        let client = RegionDataLoaderSpy()
+        let sut = RegionSelectorManager(dataLoader: client)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        return (sut, client)
+    }
 }
+
+class RegionDataLoaderSpy: RegionDataLoader{
+
+}
+
+
+
