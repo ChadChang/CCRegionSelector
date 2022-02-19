c3cfa7fe10840efe032abb21b12d8b3efe59cff8
Chad Chang <chadchang.tw@gmail.com>
2022-02-19 23:06:07 +0800

    refactor: Add TetstCCRegionSelectorSPM project for testing

---
+++ TestCCRegionSelectorSPM/TestCCRegionSelectorSPM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCCRegionSelectorSPM
+//
+//  Created by Chad Chang on 2022/2/19.
+//
+
+import UIKit
+import CCRegionSelector
+
+class ViewController: UIViewController {
+    var selectorView: RegionSelectorView?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
